d1a2c10338c6599169172a2697a9380937865371
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-06 17:43:10 +0200

    Bootstrap API with validation, CORS, Swagger, and exception filtering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { API_GLOBAL_PREFIX } from './common/constants/api.constants';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('app.port');
+
+  app.setGlobalPrefix(API_GLOBAL_PREFIX);
+  app.enableShutdownHooks();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  setupSwagger(app);
+
+  await app.listen(port);
+  logger.log(`API running on http://localhost:${port}/${API_GLOBAL_PREFIX}`);
 }
+
 bootstrap();
