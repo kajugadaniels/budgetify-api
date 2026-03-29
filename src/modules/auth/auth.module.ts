42a0986dce480454cdca2975911f3bf1a2214964
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-29 15:53:41 +0200

    Register EmailModule and OtpService in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import { EmailModule } from '../email/email.module';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { GoogleAuthService } from './services/google-auth.service';
+import { OtpService } from './services/otp.service';
 import { SessionService } from './services/session.service';
 import { TokenService } from './services/token.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -16,11 +18,13 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     JwtModule.register({}),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     UsersModule,
+    EmailModule,
   ],
   controllers: [AuthController],
   providers: [
     AuthService,
     GoogleAuthService,
+    OtpService,
     SessionService,
     TokenService,
     JwtStrategy,
