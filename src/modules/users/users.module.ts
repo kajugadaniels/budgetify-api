b1f03688725083f3b9e1ebe41d58ee5517163c0c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-29 19:49:42 +0200

    Register users profile update controller and auth guard

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersRepository, UsersService],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  controllers: [UsersController],
+  providers: [UsersRepository, UsersService, JwtAuthGuard],
   exports: [UsersRepository, UsersService],
 })
 export class UsersModule {}
