651d317d041143f8ace268a3a44f10f61bcbf9a1
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-29 21:28:08 +0200

    Add response DTO for todo records with image collections

---
+++ src/modules/todos/dto/todo-response.dto.ts
@@ -0,0 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { TodoPriority } from '@prisma/client';
+
+import { TodoImageResponseDto } from './todo-image-response.dto';
+
+export class TodoResponseDto {
+  @ApiProperty({ example: '8d65c09f-e7fa-4ee1-9428-3b1ebc80a918' })
+  id!: string;
+
+  @ApiProperty({ example: 'Renew car insurance' })
+  name!: string;
+
+  @ApiProperty({ example: 85000 })
+  price!: number;
+
+  @ApiProperty({
+    enum: TodoPriority,
+    example: TodoPriority.TOP_PRIORITY,
+  })
+  priority!: TodoPriority;
+
+  @ApiProperty({
+    example:
+      'https://res.cloudinary.com/demo/image/upload/v1711742100/todos/todo-id/sample.jpg',
+    nullable: true,
+  })
+  coverImageUrl!: string | null;
+
+  @ApiProperty({ example: 3 })
+  imageCount!: number;
+
+  @ApiProperty({
+    type: TodoImageResponseDto,
+    isArray: true,
+  })
+  images!: TodoImageResponseDto[];
+
+  @ApiProperty({ example: '2026-03-29T20:15:30.000Z' })
+  createdAt!: Date;
+
+  @ApiProperty({ example: '2026-03-29T20:15:30.000Z' })
+  updatedAt!: Date;
+}
