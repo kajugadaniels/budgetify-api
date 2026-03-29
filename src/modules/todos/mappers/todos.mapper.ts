504c9e65a3eb2fdf4dc35e1b19bb7dc8a630943e
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-29 21:28:08 +0200

    Add mapper for todo responses and primary cover image data

---
+++ src/modules/todos/mappers/todos.mapper.ts
@@ -0,0 +1,45 @@
+import { TodoImage } from '@prisma/client';
+
+import { TodoImageResponseDto } from '../dto/todo-image-response.dto';
+import { TodoResponseDto } from '../dto/todo-response.dto';
+import { TodoWithImages } from '../todos.repository';
+
+export class TodosMapper {
+  static toTodoResponse(todo: TodoWithImages): TodoResponseDto {
+    const coverImage =
+      todo.images.find((image) => image.isPrimary) ?? todo.images[0];
+
+    return {
+      id: todo.id,
+      name: todo.name,
+      price: Number(todo.price),
+      priority: todo.priority,
+      coverImageUrl: coverImage?.imageUrl ?? null,
+      imageCount: todo.images.length,
+      images: todo.images.map((image) =>
+        TodosMapper.toTodoImageResponse(image),
+      ),
+      createdAt: todo.createdAt,
+      updatedAt: todo.updatedAt,
+    };
+  }
+
+  static toTodoResponseList(todos: TodoWithImages[]): TodoResponseDto[] {
+    return todos.map((todo) => TodosMapper.toTodoResponse(todo));
+  }
+
+  private static toTodoImageResponse(image: TodoImage): TodoImageResponseDto {
+    return {
+      id: image.id,
+      imageUrl: image.imageUrl,
+      publicId: image.publicId,
+      width: image.width,
+      height: image.height,
+      bytes: image.bytes,
+      format: image.format,
+      isPrimary: image.isPrimary,
+      createdAt: image.createdAt,
+      updatedAt: image.updatedAt,
+    };
+  }
+}
