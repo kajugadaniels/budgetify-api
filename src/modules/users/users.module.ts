47c7e06062a58eae262f8c918ea0568e9249ed94
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-31 18:22:30 +0200

    Register src/modules/users/users.module.ts avatar storage service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { AvatarImageStorageService } from './services/avatar-image-storage.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
@@ -9,7 +10,12 @@ import { UsersService } from './users.service';
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [UsersController],
-  providers: [UsersRepository, UsersService, JwtAuthGuard],
+  providers: [
+    UsersRepository,
+    UsersService,
+    AvatarImageStorageService,
+    JwtAuthGuard,
+  ],
   exports: [UsersRepository, UsersService],
 })
 export class UsersModule {}
