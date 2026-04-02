f03ca6b36a8df43aeb2ab00c522113c5ca6eb687
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-02 11:27:06 +0200

    Update src/main.ts application bootstrap configuration

--- src/main.ts
+++ src/main.ts
@@ -25,9 +25,6 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
