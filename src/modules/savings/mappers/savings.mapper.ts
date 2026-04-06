12a4014d0eda0bfe845f7b21f08c68c539c90ebb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-06 21:37:48 +0200

    Update src/modules/savings/mappers/savings.mapper.ts map saving creator

--- src/modules/savings/mappers/savings.mapper.ts
+++ src/modules/savings/mappers/savings.mapper.ts
@@ -1,11 +1,10 @@
-import { Saving } from '@prisma/client';
-
 import { PaginatedResponse } from '../../../common/interfaces/paginated-response.interface';
 import { PaginatedSavingResponseDto } from '../dto/paginated-saving.response.dto';
 import { SavingResponseDto } from '../dto/saving-response.dto';
+import { SavingWithCreator } from '../savings.repository';
 
 export class SavingsMapper {
-  static toSavingResponse(saving: Saving): SavingResponseDto {
+  static toSavingResponse(saving: SavingWithCreator): SavingResponseDto {
     return {
       id: saving.id,
       label: saving.label,
@@ -15,15 +14,23 @@ export class SavingsMapper {
       stillHave: saving.stillHave,
       createdAt: saving.createdAt,
       updatedAt: saving.updatedAt,
+      createdBy: {
+        id: saving.user.id,
+        firstName: saving.user.firstName,
+        lastName: saving.user.lastName,
+        avatarUrl: saving.user.avatarUrl,
+      },
     };
   }
 
-  static toSavingResponseList(savings: Saving[]): SavingResponseDto[] {
+  static toSavingResponseList(
+    savings: SavingWithCreator[],
+  ): SavingResponseDto[] {
     return savings.map((saving) => SavingsMapper.toSavingResponse(saving));
   }
 
   static toPaginatedSavingResponse(
-    payload: PaginatedResponse<Saving>,
+    payload: PaginatedResponse<SavingWithCreator>,
   ): PaginatedSavingResponseDto {
     return {
       items: SavingsMapper.toSavingResponseList(payload.items),
