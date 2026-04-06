844297bd428aedea9a257769b221c245b1d58f38
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-06 21:38:04 +0200

    Update src/modules/todos/todos.module.ts import partnerships module

--- src/modules/todos/todos.module.ts
+++ src/modules/todos/todos.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
 import { TodoImageStorageService } from './services/todo-image-storage.service';
 import { TodosController } from './todos.controller';
@@ -9,7 +10,11 @@ import { TodosRepository } from './todos.repository';
 import { TodosService } from './todos.service';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), UsersModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    UsersModule,
+    PartnershipsModule,
+  ],
   controllers: [TodosController],
   providers: [
     TodosRepository,
