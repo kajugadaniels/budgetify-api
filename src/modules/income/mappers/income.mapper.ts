83189fffddc89e76f7edf1551c034a21eada3bf2
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-06 21:37:40 +0200

    Update src/modules/income/mappers/income.mapper.ts map income creator

--- src/modules/income/mappers/income.mapper.ts
+++ src/modules/income/mappers/income.mapper.ts
@@ -1,11 +1,10 @@
-import { Income } from '@prisma/client';
-
 import { PaginatedResponse } from '../../../common/interfaces/paginated-response.interface';
 import { PaginatedIncomeResponseDto } from '../dto/paginated-income.response.dto';
 import { IncomeResponseDto } from '../dto/income-response.dto';
+import { IncomeWithCreator } from '../income.repository';
 
 export class IncomeMapper {
-  static toIncomeResponse(income: Income): IncomeResponseDto {
+  static toIncomeResponse(income: IncomeWithCreator): IncomeResponseDto {
     return {
       id: income.id,
       label: income.label,
@@ -15,15 +14,23 @@ export class IncomeMapper {
       received: income.received,
       createdAt: income.createdAt,
       updatedAt: income.updatedAt,
+      createdBy: {
+        id: income.user.id,
+        firstName: income.user.firstName,
+        lastName: income.user.lastName,
+        avatarUrl: income.user.avatarUrl,
+      },
     };
   }
 
-  static toIncomeResponseList(incomes: Income[]): IncomeResponseDto[] {
+  static toIncomeResponseList(
+    incomes: IncomeWithCreator[],
+  ): IncomeResponseDto[] {
     return incomes.map((income) => IncomeMapper.toIncomeResponse(income));
   }
 
   static toPaginatedIncomeResponse(
-    payload: PaginatedResponse<Income>,
+    payload: PaginatedResponse<IncomeWithCreator>,
   ): PaginatedIncomeResponseDto {
     return {
       items: IncomeMapper.toIncomeResponseList(payload.items),
