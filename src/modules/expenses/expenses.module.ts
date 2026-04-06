563e93544587dcec8b217be54866eada73a137cd
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-06 21:37:33 +0200

    Update src/modules/expenses/expenses.module.ts import partnerships module

--- src/modules/expenses/expenses.module.ts
+++ src/modules/expenses/expenses.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesRepository } from './expenses.repository';
 import { ExpensesService } from './expenses.service';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), UsersModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    UsersModule,
+    PartnershipsModule,
+  ],
   controllers: [ExpensesController],
   providers: [ExpensesRepository, ExpensesService, JwtAuthGuard],
   exports: [ExpensesService],
