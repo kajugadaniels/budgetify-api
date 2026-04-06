836b71d03d30026ee27d5ac984c00cf4db5890ad
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-06 21:38:17 +0200

    Add src/modules/partnerships/partnerships.routes.ts partnership routes

---
+++ src/modules/partnerships/partnerships.routes.ts
@@ -0,0 +1,7 @@
+export const PARTNERSHIPS_ROUTES = {
+  root: 'partnerships',
+  invite: 'partnerships/invite',
+  inviteInfo: 'partnerships/invite-info',
+  accept: 'partnerships/accept',
+  mine: 'partnerships/mine',
+} as const;
