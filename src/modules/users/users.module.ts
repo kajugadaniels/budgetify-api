d3ed75b143d09e989a3a567c70f7c62b99e4c496
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-07 22:54:31 +0200

    Update src/modules/users/users.module.ts register deletion lifecycle service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,18 +2,21 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { EmailModule } from '../email/email.module';
+import { AccountDeletionLifecycleService } from './services/account-deletion-lifecycle.service';
 import { AvatarImageStorageService } from './services/avatar-image-storage.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), EmailModule],
   controllers: [UsersController],
   providers: [
     UsersRepository,
     UsersService,
     AvatarImageStorageService,
+    AccountDeletionLifecycleService,
     JwtAuthGuard,
   ],
   exports: [UsersRepository, UsersService],
