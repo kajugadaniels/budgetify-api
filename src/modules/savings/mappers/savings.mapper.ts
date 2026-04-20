c7f7d0b26858b497ee4b9cefa671980858e69ddb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-20 22:22:36 +0200

    Calculate saving balances from ledger

--- src/modules/savings/mappers/savings.mapper.ts
+++ src/modules/savings/mappers/savings.mapper.ts
@@ -1,3 +1,5 @@
+import { SavingTransactionType } from '@prisma/client';
+
 import { PaginatedResponse } from '../../../common/interfaces/paginated-response.interface';
 import { PaginatedSavingResponseDto } from '../dto/paginated-saving.response.dto';
 import { SavingResponseDto } from '../dto/saving-response.dto';
@@ -5,12 +7,43 @@ import { SavingWithCreator } from '../savings.repository';
 
 export class SavingsMapper {
   static toSavingResponse(saving: SavingWithCreator): SavingResponseDto {
+    const totals = saving.transactions.reduce(
+      (accumulator, transaction) => {
+        const amountRwf = Number(transaction.amountRwf);
+
+        if (transaction.type === SavingTransactionType.DEPOSIT) {
+          accumulator.totalDepositedRwf += amountRwf;
+        }
+
+        if (transaction.type === SavingTransactionType.WITHDRAWAL) {
+          accumulator.totalWithdrawnRwf += amountRwf;
+        }
+
+        if (transaction.type === SavingTransactionType.ADJUSTMENT) {
+          accumulator.adjustmentRwf += amountRwf;
+        }
+
+        return accumulator;
+      },
+      {
+        totalDepositedRwf: 0,
+        totalWithdrawnRwf: 0,
+        adjustmentRwf: 0,
+      },
+    );
+
     return {
       id: saving.id,
       label: saving.label,
       amount: Number(saving.amount),
       currency: saving.currency,
       amountRwf: Number(saving.amountRwf),
+      totalDepositedRwf: totals.totalDepositedRwf,
+      totalWithdrawnRwf: totals.totalWithdrawnRwf,
+      currentBalanceRwf:
+        totals.totalDepositedRwf -
+        totals.totalWithdrawnRwf +
+        totals.adjustmentRwf,
       date: saving.date,
       note: saving.note,
       stillHave: saving.stillHave,
