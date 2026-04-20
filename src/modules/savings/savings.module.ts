f97012285ec1cc28bfedcc5e55b602067d8a96d3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-20 22:11:08 +0200

    Provide currency conversion to savings module

--- src/modules/savings/savings.module.ts
+++ src/modules/savings/savings.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrencyModule } from '../currency/currency.module';
 import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
 import { SavingsController } from './savings.controller';
@@ -11,6 +12,7 @@ import { SavingsService } from './savings.service';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    CurrencyModule,
     UsersModule,
     PartnershipsModule,
   ],
