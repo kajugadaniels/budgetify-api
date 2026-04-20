752ce6b14e34e1aca1cfa94beeadcb9680013317
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-20 22:11:02 +0200

    Provide currency conversion to income module

--- src/modules/income/income.module.ts
+++ src/modules/income/income.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrencyModule } from '../currency/currency.module';
 import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
 import { IncomeController } from './income.controller';
@@ -11,6 +12,7 @@ import { IncomeService } from './income.service';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    CurrencyModule,
     UsersModule,
     PartnershipsModule,
   ],
