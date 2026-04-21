2cf2a9b0e1de2e6657567860b0be75cc15d7ff91
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-21 12:37:44 +0200

    Return income allocation fields in list responses

--- src/modules/income/dto/income-response.dto.ts
+++ src/modules/income/dto/income-response.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Currency, IncomeCategory } from '@prisma/client';
 
 import { CreatedByResponseDto } from '../../../common/dto/created-by.response.dto';
+import { IncomeAllocationStatus } from './income-allocation-status.enum';
 
 export class IncomeResponseDto {
   @ApiProperty({ example: '8d65c09f-e7fa-4ee1-9428-3b1ebc80a918' })
@@ -41,6 +42,27 @@ export class IncomeResponseDto {
     description: 'Whether this income has already been received.',
   })
   received!: boolean;
+
+  @ApiProperty({
+    example: 120000,
+    description:
+      'How much of this income has already been allocated into savings.',
+  })
+  allocatedToSavingsRwf!: number;
+
+  @ApiProperty({
+    example: 330000,
+    description:
+      'How much of this income is still free after savings allocations.',
+  })
+  remainingAvailableRwf!: number;
+
+  @ApiProperty({
+    enum: IncomeAllocationStatus,
+    enumName: 'IncomeAllocationStatus',
+    example: IncomeAllocationStatus.PARTIALLY_ALLOCATED,
+  })
+  allocationStatus!: IncomeAllocationStatus;
 
   @ApiProperty({ example: '2026-03-29T19:30:00.000Z' })
   createdAt!: Date;
