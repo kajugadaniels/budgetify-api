fd93de23f32b0d899bcddcca90345f3f85c844c3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-21 04:23:27 +0200

    Import summary dependencies into income module

--- src/modules/income/income.module.ts
+++ src/modules/income/income.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CurrencyModule } from '../currency/currency.module';
+import { ExpensesModule } from '../expenses/expenses.module';
 import { PartnershipsModule } from '../partnerships/partnerships.module';
+import { SavingsModule } from '../savings/savings.module';
 import { UsersModule } from '../users/users.module';
 import { IncomeController } from './income.controller';
 import { IncomeRepository } from './income.repository';
@@ -13,8 +15,10 @@ import { IncomeService } from './income.service';
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     CurrencyModule,
+    ExpensesModule,
     UsersModule,
     PartnershipsModule,
+    forwardRef(() => SavingsModule),
   ],
   controllers: [IncomeController],
   providers: [IncomeRepository, IncomeService, JwtAuthGuard],
