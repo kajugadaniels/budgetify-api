6d4415bc19966e117c3746f06295c9e602f6d205
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-21 19:12:39 +0200

    Wire expense tariff and income summary dependencies

--- src/modules/expenses/expenses.module.ts
+++ src/modules/expenses/expenses.module.ts
@@ -1,21 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrencyModule } from '../currency/currency.module';
+import { IncomeModule } from '../income/income.module';
 import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesRepository } from './expenses.repository';
 import { ExpensesService } from './expenses.service';
+import { MobileMoneyTariffService } from './mobile-money-tariff.service';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    CurrencyModule,
+    forwardRef(() => IncomeModule),
     UsersModule,
     PartnershipsModule,
   ],
   controllers: [ExpensesController],
-  providers: [ExpensesRepository, ExpensesService, JwtAuthGuard],
+  providers: [
+    ExpensesRepository,
+    ExpensesService,
+    MobileMoneyTariffService,
+    JwtAuthGuard,
+  ],
   exports: [ExpensesService, ExpensesRepository],
 })
 export class ExpensesModule {}
