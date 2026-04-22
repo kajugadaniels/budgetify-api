dd51301ad848c875d237845da07eab96b52831af
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-22 20:32:38 +0200

    Wire expense dependencies into the todos module

--- src/modules/todos/todos.module.ts
+++ src/modules/todos/todos.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ExpensesModule } from '../expenses/expenses.module';
 import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
 import { TodoImageStorageService } from './services/todo-image-storage.service';
@@ -12,6 +13,7 @@ import { TodosService } from './todos.service';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    ExpensesModule,
     UsersModule,
     PartnershipsModule,
   ],
