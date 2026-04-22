4b45aead4dcd727464fdcf35b5039faf608d05bf
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-22 20:35:14 +0200

    Add todo recording routes

--- src/modules/todos/todos.routes.ts
+++ src/modules/todos/todos.routes.ts
@@ -1,6 +1,7 @@
 export const TODOS_ROUTES = {
   base: 'todos',
   byId: ':todoId',
+  recordings: ':todoId/recordings',
   images: ':todoId/images',
   imageById: ':todoId/images/:imageId',
 } as const;
