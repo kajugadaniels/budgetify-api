addacae2af4a03b903a49ca95c4c48e738bfd71d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-24 17:34:40 +0200

    Wire currency conversion support into the loans module

--- src/modules/loans/loans.module.ts
+++ src/modules/loans/loans.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrencyModule } from '../currency/currency.module';
 import { ExpensesRepository } from '../expenses/expenses.repository';
 import { PartnershipsModule } from '../partnerships/partnerships.module';
 import { UsersModule } from '../users/users.module';
@@ -12,6 +13,7 @@ import { LoansService } from './loans.service';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    CurrencyModule,
     UsersModule,
     PartnershipsModule,
   ],
