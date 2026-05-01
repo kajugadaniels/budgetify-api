b67c512790848542acf825c7ec9a5142bc1d8c87
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-05-01 14:57:48 +0200

    Register the loan transaction reversal route

--- src/modules/loans/loans.routes.ts
+++ src/modules/loans/loans.routes.ts
@@ -2,6 +2,7 @@ export const LOANS_ROUTES = {
   base: 'loans',
   byId: ':loanId',
   transactions: ':loanId/transactions',
+  reverseTransaction: ':loanId/transactions/:transactionId/reverse',
   transactionToExpense: ':loanId/transactions/:transactionId/send-to-expense',
   transactionToIncome: ':loanId/transactions/:transactionId/send-to-income',
   sendToExpense: ':loanId/send-to-expense',
